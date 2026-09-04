c324aa7d228179c52d1fd166441f8c5358806792
Mila <dinges.milada.andreevna@gmail.com>
2026-09-04 23:48:19 +0700

    feat: начала делать фильтры у каталога(категории, выпадающие списки)

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,16 +1,99 @@
 'use client'
+
 import { use, useState } from "react";
-import { Costume } from "@/types/costume";
+
+import { Costume, SIZE_ARRAY, SizeFilter, CATEGORIES_ARRAY, CategoryFilter, GENDER_ARRAY, GenderFilter, SUBCATEGORIES_ARRAY, SubcategoryFilter } from "@/types/costume";
 
 interface CatalogProps{
     initialCostumes: Costume[];
 }
 
-export default function Catalog({initialCostumes}: CatalogProps) {
-    const [costumes] = useState<Costume[]>(initialCostumes)
-  return (
-    <section className="catalog">
-        <h2>Успешно! {costumes.length} шт</h2>
-    </section>
-  );
+export default function Catalog({ initialCostumes }: CatalogProps) {
+    const [costumes] = useState<Costume[]>(initialCostumes);
+    const [selectedCategory, setSelectedCategory] = useState<CategoryFilter>('Все категории');
+    const [selectedSubcategory, setSelectedSubcategory] = useState<SubcategoryFilter>('Все подкатегории');
+    const [selectedSize, setSelectedSize] = useState<SizeFilter>('Размер');
+    const [selectedGender, setSelectedGender] = useState<GenderFilter>('Пол');
+
+    const [activeDropdown, setActiveDropdown] = useState< 'sub' | 'size' | 'gender' | null>(null);
+
+    const categoryBtns = ['Все категории', ...CATEGORIES_ARRAY] as const;
+    const subcategoryBtns = ['Все подкатегории', ...SUBCATEGORIES_ARRAY] as const;
+    const sizeBtns = ['Размер', ...SIZE_ARRAY] as const;
+    const genderBtns = ['Пол', ...GENDER_ARRAY] as const;
+
+    return (
+        <section className="catalog">
+            <div className="catalog__filters">
+                <div className="filters__category">
+                    {categoryBtns.map((btnName) => (
+                        <button key={btnName} 
+                        className={`filters__category-btn ${selectedCategory === btnName ? 'category-btn__active' : ''}`}
+                        onClick={() => setSelectedCategory(btnName)}>
+                            {btnName}
+                        </button>
+                        ))
+                    }
+                </div>
+            </div> 
+            {selectedCategory === 'Карнавальные' && (
+                <div className="filters__carnaval">
+                    <div className="carnaval__sub-pc">
+                    {subcategoryBtns.map((btnName) => (
+                        <button key={btnName}
+                        className={`sub-btn ${selectedSubcategory === btnName ? 'sub-btn__active' : ''}`}
+                        onClick={() => setSelectedSubcategory(btnName)}>
+                            {btnName}
+                        </button>
+                        ))
+                    }
+                    </div>
+                    
+                /////////////////////////выпадающие списки///////////////////////////////////////////////////////////////////////
+                    <div className="carnaval-sub">
+                        <button className={`dropdown-toggle ${selectedSubcategory !== 'Все подкатегории' ? 'dropdown-toggle__active' : ''}`}
+                        onClick={() => setActiveDropdown(activeDropdown === 'sub' ? null : 'sub')}>
+                            {selectedSubcategory}
+                        </button>
+                    {activeDropdown === 'sub' && (
+                        <ul className="carnaval-sub__dropdown">
+                            {subcategoryBtns.map((liName) => (
+                                <li key={liName} className="dropdown__li">
+                                <button className='dropdown__btn' 
+                                onClick={() => {setSelectedSubcategory(liName); setActiveDropdown(null);}}>
+                                    {liName}
+                                </button>
+                                </li>
+                            ))}
+                        </ul>
+                        )}
+
+                    </div>
+
+                    <div className="carnaval__size">
+                    {sizeBtns.map((liName) => (
+                        <button key={liName}
+                        className={`dropdown-btn ${selectedSize === liName ? 'dropdown__active' : ''}`}
+                        onClick={() => setSelectedSize(liName)}>
+                            {liName}
+                        </button>
+                        ))
+                    }
+                    </div>
+
+                    <div className="carnaval__gender">
+                    {genderBtns.map((liName) => (
+                        <button key={liName}
+                        className={`dropdown-btn ${selectedGender === liName ? 'dropdown__active' : ''}`}
+                        onClick={() => setSelectedGender(liName)}>
+                            {liName}
+                        </button>
+                        ))
+                    }
+                    </div>
+                </div>
+                )
+            }
+        </section>
+    );
 }
--- src/types/costume.ts
+++ src/types/costume.ts
@@ -1,19 +1,30 @@
-type Size = '42' | '44' | '46' | '48' | '50' | '52' | '54' | '56' | '58' | '60' | '62';
 
-type Gender = 'Женский' | 'Мужской';
-export type GenderFilter = 'Не выбрано' | Gender;
+export const SIZE_ARRAY = ['42', '44', '46', '48', '50', '52', '54', '56', '58', '60', '62'] as const;
+type Size = typeof SIZE_ARRAY[number];
+export type SizeFilter = 'Размер' | Size;
 
-type Category = 'Карнавальные' | 'Ростовые куклы' | 'Ростовые-наудвные';
-export type CategoryFilter = 'Все' | Category;
+
+export const GENDER_ARRAY = ['Женский', 'Мужской'] as const;
+type Gender = typeof GENDER_ARRAY[number];
+export type GenderFilter = 'Пол' | Gender;
+
+export const CATEGORIES_ARRAY = ['Карнавальные', 'Ростовые куклы', 'Ростовые-наудвные'] as const;
+type Category = typeof CATEGORIES_ARRAY[number];
+export type CategoryFilter = 'Все категории' | Category;
+
+export const SUBCATEGORIES_ARRAY = ['Народные', 'Сказка', 'Герои',  'Злодеи', 'Воин', 'Хеллоуин', 'Готика', 'Веселье', 'Приключения', '80-е и 90-е', 'Диско', 'Великий Гэтсби', 'Новый год', 'Исторические', 'Вечерние' , 'Прочее'] as const;
+type Subcategory = typeof SUBCATEGORIES_ARRAY[number];
+export type SubcategoryFilter = 'Все подкатегории' | Subcategory;
+
 
 export interface Costume {
   id: string;               
   title: string;            
   category: Category; 
-  subcategory: string;     
-  gender: Gender;     
-  sizes: Size[];          
+  subcategory?: Subcategory;     
+  gender?: Gender[];     
+  sizes?: Size[];          
   pricePerDay: number;      
   imageUrl: string;         
-  description: string;     
+  description?: string;     
 }
